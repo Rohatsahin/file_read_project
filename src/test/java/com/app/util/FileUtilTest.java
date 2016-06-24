2a60dfce537cae45b0e3c88301f019b46bcef155
Rohatsahin <rohatsahin92@gmail.com>
2016-06-24 11:19:54 +0300

    another development commit

--- src/test/java/com/app/util/FileUtilTest.java
+++ src/test/java/com/app/util/FileUtilTest.java
@@ -1,32 +1,53 @@
 package com.app.util;
 
+import org.junit.Before;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.TemporaryFolder;
 
+import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.IOException;
 
 import static org.junit.Assert.*;
 
 public class FileUtilTest {
 
+    private String file_part;
+
+    private String dummy_part = "D:\\proje\\src\\test\\resources\\Rohat Şahin--CV.docx";
+
+    @Rule
+    public TemporaryFolder temporaryFolder = new TemporaryFolder();
+
+    @Before
+    public void setUp() throws IOException {
+        File file = temporaryFolder.newFile("test.pdf");
+        file_part = file.getPath();
+    }
+
     @Test
-    public void shouldReturnTrueIfFileExist(){
-        String file_part = "D:\\proje\\src\\test\\resources\\Rohat Şahin--CV.docx";
+    public void shouldReturnTrueIfFileExist() throws IOException{
 
         assertTrue(FileUtil.checkFileExists(file_part));
     }
 
     @Test
     public void shouldReturnFalseIfFileNotExist(){
-        String file_part = "D:\\proje\\src\\test\\resources\\Rohat Şahin--CV.pdf";
 
-        assertFalse(FileUtil.checkFileExists(file_part));
+        assertFalse(FileUtil.checkFileExists(dummy_part));
     }
 
-    @Test(expected = NullPointerException.class)
-    public void shouldThrowFileNotFoundException(){
-        String file_part = "D:\\proje\\src\\test\\resources\\Rohat Şahin--CV.pdf";
-        
-        assertThat(FileUtil.createFileStream(file_part),null);
+    @Test
+    public void shouldReturnNullWhenFileNotExist(){
+
+        assertNull(FileUtil.createFileStream(dummy_part));
+    }
+
+    @Test(expected = FileNotFoundException.class)
+    public void shouldReturnFileNotFoundException(){
+
+        FileUtil.createFileStream(file_part);
     }
 
 }
